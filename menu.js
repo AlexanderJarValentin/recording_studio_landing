cb813f2cc6f35ba4f13433f86d699bb38b2f7885
Walek-dot <chertograd114@gmail.com>
2021-05-03 02:52:27 +0300

    + slider 03.05

--- menu.js
+++ menu.js
@@ -4,3 +4,25 @@ $(document).ready(function() {
         $('.header__nav').toggleClass('open-menu');
     });
 });
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
