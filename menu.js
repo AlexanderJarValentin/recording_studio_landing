b60bff15257aa548903205257affec770d9b372a
Walek-dot <chertograd114@gmail.com>
2021-05-04 19:14:08 +0300

    popup new 04.05

--- menu.js
+++ menu.js
@@ -2,7 +2,26 @@ $(document).ready(function() {
     $('.header__burger').click(function() {
         $('.header__burger').toggleClass('open-menu');
         $('.header__nav').toggleClass('open-menu');
+        
+        
     });
+});
+
+$(".form-popup").click(function() {
+  $("#form").toggleClass('open__form'); 
+  
+});
+
+$(".price-popup").click(function() {
+  $(".work__sale").toggleClass('open__sale') ;
+  //$(".btn__block").hide() ;
+  //$(".block__work").hide() ;
+});
+
+$(".hide-popup").click(function() {
+  //$(".work__sale").toggleClass('off__sale') ;
+  //$(".btn__block").show() ;
+ //$(".block__work").show() ;
 });
 
 const swiper = new Swiper('.swiper-container', {
